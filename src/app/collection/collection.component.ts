18f309605917c6d844fdaf84322930ff273d3e20
Michel Pereira dos Santos <michel.santos@e-construmarket.com.br>
2024-12-29 03:35:18 -0300

    parcialmente

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -1,11 +1,124 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+import { CollectionService } from './services/collection.service';
+
+export interface CardData {
+  state: 'default' | 'flipped' | 'matched';
+}
 
 @Component({
   selector: 'app-collection',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './collection.component.html',
-  styleUrl: './collection.component.scss'
+  styleUrl: './collection.component.scss',
+  animations: [
+    trigger('cardFlip', [
+      state(
+        'default',
+        style({
+          transform: 'none',
+        })
+      ),
+      state(
+        'flipped',
+        style({
+          transform: 'rotateY(180deg)',
+        })
+      ),
+      state(
+        'matched',
+        style({
+          visibility: 'false',
+          transform: 'scale(0.05)',
+          opacity: 0,
+        })
+      ),
+      transition('default => flipped', [animate('400ms')]),
+      transition('flipped => default', [animate('400ms')]),
+      transition('* => matched', [animate('400ms')]),
+    ]),
+  ],
 })
-export class CollectionComponent {
+export class CollectionComponent implements OnInit {
+  @Output() backToMenu = new EventEmitter<boolean>();
 
+  public cardList: any[] = [];
+  public spinCards: any[] = [];
+  public loading: boolean = true;
+
+  data: CardData[] = [
+    {
+      state: 'default',
+    },
+    {
+      state: 'default',
+    },
+    {
+      state: 'default',
+    },
+    {
+      state: 'default',
+    },
+    {
+      state: 'default',
+    },
+  ];
+
+  constructor(private collectionService: CollectionService) {}
+  ngOnInit(): void {
+    this.getAllCards();
+  }
+
+  toggleFlip(i: number) {
+    if (this.data[i].state === 'default') {
+      this.data[i].state = 'flipped';
+    }
+  }
+
+  getCardsFromSpin() {
+    this.collectionService.spinFirstCollection().subscribe((res: any) => {
+      this.spinCards = res.cardList;
+      this.saveNewCards();
+    });
+  }
+
+  saveNewCards() {
+    const groupList = this.spinCards.map((card) => card.group);
+    this.collectionService.register(groupList).subscribe({
+      next: () => {},
+      error: (err) => {
+        console.error('Error:', err);
+      },
+    });
+  }
+
+  backToCollection() {
+    this.getAllCards();
+  }
+
+  getAllCards() {
+    this.collectionService.getAllCards().subscribe((res: any) => {
+      this.loading = false;
+      this.cardList = res.cardList;
+    });
+  }
+
+  getImgUrl(group: string) {
+    return `assets/cars/${group.toUpperCase()}.jpg`
+  }
 }
--- src/app/collection/services/collection.service.ts
+++ src/app/collection/services/collection.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CollectionService {
+  private apiUrl = 'http://localhost:5000/api/cardUser';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  register(groups: Number[]): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post(`${this.apiUrl}/register`, groups, { headers });
+  }
+
+  getAllCards() {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get(`${this.apiUrl}/getAllByUser`, { headers });
+  }
+
+  spinFirstCollection() {
+    return this.http.get(`${this.apiUrl}/getInitialCollection`);
+  }
 }
